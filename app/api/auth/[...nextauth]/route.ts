7c75577fbcfd5b9e49c76adf72a309ccb02c9d15
gerdazk <gerda.zykute@gmail.com>
2024-03-10 20:23:54 +0200

    Remove comments

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,3 @@
-// import { authenticate } from "@/services/authService"
 import NextAuth from "next-auth"
 import type { AuthOptions } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
@@ -18,17 +17,12 @@ export const authOptions: AuthOptions = {
         password: { label: "Password", type: "password" }
       },
       async authorize(credentials, req) {
-        // Add logic here to look up the user from the credentials supplied
         const user = await loginUser({...credentials})
   
         if (user) {
-          // Any object returned will be saved in `user` property of the JWT
           return user
         } else {
-          // If you return null then an error will be displayed advising the user to check their details.
           return null
-  
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
         }
       }
     })
