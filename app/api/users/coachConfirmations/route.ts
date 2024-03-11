5c80af8e1223f8e725aa9e1e92444bcddfb65cb6
gerdazk <gerda.zykute@gmail.com>
2024-03-11 18:11:52 +0200

    Add coach confirmation logic

--- app/api/users/coachConfirmations/route.ts
+++ app/api/users/coachConfirmations/route.ts
@@ -19,3 +19,22 @@ export async function GET(req) {
        return  NextResponse.json({ success: false, error: 'Error fetching users', status: 500 });
       }
 }
+
+export async function PATCH(req) {
+
+  const { email } = await req.json();
+  const prisma = new PrismaClient()
+
+  try {
+    const updatedUser = await prisma.user.update({
+      where: { email },
+      data: { role: 'coach' },
+    });
+
+    return NextResponse.json({ success: true, user: updatedUser, status: 200 });
+  } catch (error) {
+    console.error('Error updating user role:', error);
+   return NextResponse.json({ success: false, error: 'Error updating user role', status: 500 });
+  }
+
+}
--- app/users/coachConfirmations/page.tsx
+++ app/users/coachConfirmations/page.tsx
@@ -1,7 +1,42 @@
+'use client'
+
+import { getCoachesAwaitingConfirmation } from "@/src/utils/getCoachesAwaitingConfirmation"
+import { useEffect, useState } from "react"
+import { SingleCoachConfirmation } from "./components/SingleCoachConfirmation"
+import { PageHeader } from "@/src/components/PageHeader"
+
 export default function Page() {
+
+    const [coaches, setCoaches] = useState([])
+
+    const getCoaches = async () => {
+        const allAwaitingCoaches = await getCoachesAwaitingConfirmation()
+        console.log({allAwaitingCoaches})
+        allAwaitingCoaches && setCoaches(allAwaitingCoaches.users)
+    }
+
+
+    useEffect(() => {
+
+        getCoaches()
+        
+    }, [])
+
+    
+
     return (
         <div>
-        Coach confirmations
+
+<PageHeader title="Coaches awaiting confirmation" subtitle="All coaches with unconfirmed profiles." />
+
+<div className="flex gap-3 w-full flex-col">
+            
+                {coaches?.map(({email, name}) => {
+                    return  <SingleCoachConfirmation name={name} email={email} key={email} />
+                })}
+
+</div>
+            
         </div>
     )
 }
