91b7b6f23fde82947fe342d6a5252a48840c7905
Martin Martijan <martin.martijan@gmail.com>
2024-04-05 20:07:12 +0300

    Added rating to profile

--- app/users/profile/page.tsx
+++ app/users/profile/page.tsx
@@ -10,6 +10,10 @@ export default function Profile() {
 	const email = useSearchParams().get('email');
 	const [user, setUser] = useState<UserData | undefined>();
 
+	let rating = 0;
+	const maxRating = 5;
+	const filledStars = Math.round(rating * 2) / 2;
+
 	const getUserInfo = async () => {
 		try {
 			const userInfo = await getUser(email);
@@ -55,7 +59,25 @@ export default function Profile() {
 						</>
 					)}
 					<h2 className='scroll-m-20 text-2xl font-semibold tracking-tight'>My rating:</h2>
-					<h3 className='scroll-m-20 text-xl font-normal tracking-tight'>TO BE IMPLEMENTED</h3>
+					<div className='flex items-center'>
+						{[...Array(maxRating)].map((_, index) => (
+							<svg
+								key={index}
+								className={`w-4 h-4 ${index < filledStars ? 'text-yellow-300' : 'text-gray-300'} me-1`}
+								aria-hidden='true'
+								xmlns='http://www.w3.org/2000/svg'
+								fill='currentColor'
+								viewBox='0 0 22 20'
+							>
+								<path d='M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z' />
+							</svg>
+						))}
+						<p className='ms-1 text-sm font-medium text-gray-500 dark:text-gray-400'>{rating}</p>
+						<p className='ms-1 text-sm font-medium text-gray-500 dark:text-gray-400'>out of</p>
+						<p className='ms-1 text-sm font-medium text-gray-500 dark:text-gray-400'>{maxRating}</p>
+						<span className='w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400'></span>
+						<p className='ms-1 text-sm font-medium text-gray-500 dark:text-gray-400'>0 reviews</p>
+					</div>
 					<h2 className='scroll-m-20 text-2xl font-semibold tracking-tight'>My sessions:</h2>
 					<h3 className='scroll-m-20 text-xl font-normal tracking-tight'>TO BE IMPLEMENTED</h3>
 					{user.role === 'coach' && (
