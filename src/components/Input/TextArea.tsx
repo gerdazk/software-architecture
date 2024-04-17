bcf81e49bdedc54b2d2379295e2fae129033b6f3
Martin Martijan <martin.martijan@gmail.com>
2024-04-17 12:08:33 +0300

    Added update functionality from coach profile

--- src/components/Input/TextArea.tsx
+++ src/components/Input/TextArea.tsx
@@ -1,53 +1,35 @@
-import * as React from 'react'
-import {
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage
-} from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
-import { Textarea } from '@/components/ui/textarea'
+import * as React from 'react';
+import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
 
 type Props = {
-  placeholder?: string
-  control: any
-  name: string
-  label: string
-  description: string
-  rows: number
-  width?: string
-}
+	placeholder?: string;
+	value: string;
+	control: any;
+	name: string;
+	label: string;
+	description: string;
+	rows: number;
+	width?: string;
+};
 
-export const TextArea = ({
-  placeholder,
-  control,
-  name,
-  label,
-  description,
-  rows,
-  width = 'full'
-}: Props) => {
-  return (
-    <FormField
-      control={control}
-      name={name}
-      render={({ field }) => (
-        <FormItem>
-          <FormLabel>{label}</FormLabel>
-          <FormControl>
-            <Textarea
-              placeholder={placeholder}
-              {...field}
-              className={`w-${width}`}
-              rows={rows}
-            />
-          </FormControl>
-          <FormDescription>{description}</FormDescription>
-          <FormMessage />
-        </FormItem>
-      )}
-    />
-  )
-}
+export const TextArea = ({ placeholder, value, control, name, label, description, rows, width = 'full' }: Props) => {
+	return (
+		<FormField
+			control={control}
+			name={name}
+			defaultValue={value}
+			render={({ field }) => (
+				<FormItem>
+					<FormLabel>{label}</FormLabel>
+					<FormControl>
+						<Textarea placeholder={placeholder} {...field} className={`w-${width}`} rows={rows} />
+					</FormControl>
+					<FormDescription>{description}</FormDescription>
+					<FormMessage />
+				</FormItem>
+			)}
+		/>
+	);
+};
